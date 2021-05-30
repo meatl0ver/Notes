697758958931427e38789a8236a4050dccb3e202
meatl0ver <raulimashev@yandex.ru>
2021-05-30 15:34:04 +0300

    tenth homework

--- app/src/main/java/raui/imashev/homeworkandroid2/ui/NoteEditFragment.java
+++ app/src/main/java/raui/imashev/homeworkandroid2/ui/NoteEditFragment.java
@@ -11,17 +11,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
+import java.util.Objects;
+
+import raui.imashev.homeworkandroid2.MainActivity;
 import raui.imashev.homeworkandroid2.R;
 import raui.imashev.homeworkandroid2.data.Note;
 
+import static raui.imashev.homeworkandroid2.MainActivity.COLLECTION_NAME;
+import static raui.imashev.homeworkandroid2.MainActivity.CREATE_NEW_NOTE;
 import static raui.imashev.homeworkandroid2.MainActivity.CURRENT_NOTE_KEY;
 import static raui.imashev.homeworkandroid2.MainActivity.CURRENT_NOTE_NUMBER;
+import static raui.imashev.homeworkandroid2.MainActivity.PRIORITY_HIGHT;
+import static raui.imashev.homeworkandroid2.MainActivity.PRIORITY_LOW;
+import static raui.imashev.homeworkandroid2.MainActivity.PRIORITY_MIDDLE;
+import static raui.imashev.homeworkandroid2.MainActivity.db;
 
 
 public class NoteEditFragment extends Fragment {
 
     int currentNote;
-
+    String currentId;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -36,21 +45,24 @@ public class NoteEditFragment extends Fragment {
         Button button = view.findViewById(R.id.button);
 
         Bundle bundle = this.getArguments();
+        assert bundle != null;
         currentNote = bundle.getInt(CURRENT_NOTE_NUMBER, -1);
-        if (currentNote != -2) {
+
+        if (currentNote != CREATE_NEW_NOTE) {
             Note note = bundle.getParcelable(CURRENT_NOTE_KEY);
+            currentId = note.getId();
             editTextTitle.setText(note.getTitle());
             editTextDescription.setText(note.getDescription());
             editTextDateOfCreate.setText(note.getDateOfCreate());
             editTextDate.setText(note.getDate());
             switch (note.getPriority()) {
-                case 1:
+                case PRIORITY_HIGHT:
                     editTextTitle.setBackgroundResource(R.color.red);
                     break;
-                case 2:
+                case PRIORITY_MIDDLE:
                     editTextTitle.setBackgroundResource(R.color.yellow);
                     break;
-                case 3:
+                case PRIORITY_LOW:
                     editTextTitle.setBackgroundResource(R.color.green);
                     break;
             }
@@ -65,14 +77,29 @@ public class NoteEditFragment extends Fragment {
             note.setDateOfCreate(editTextDateOfCreate.getText().toString());
             note.setTitle(editTextTitle.getText().toString());
             if (radioButtonHightPriority.isChecked()) {
-                note.setPriority(1);
+                note.setPriority(PRIORITY_HIGHT);
             }
             if (radioButtonMiddlePriority.isChecked()) {
-                note.setPriority(2);
+                note.setPriority(PRIORITY_MIDDLE);
             }
             if (radioButtonLowPriority.isChecked()) {
-                note.setPriority(3);
+                note.setPriority(PRIORITY_LOW);
             }
+
+            if (currentNote==CREATE_NEW_NOTE){
+                note.setId(""+Math.random()* 1000);
+            } else {
+                note.setId(currentId);
+            }
+            db.collection(COLLECTION_NAME).document(note.getId()).set(note);
+
+            MainActivity.downloadData();
+            NotesFragment nextFrag = new NotesFragment();
+            Objects.requireNonNull(getActivity()).getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.notes_fragment, nextFrag, "findThisFragment")
+                    .addToBackStack(null)
+                    .commit();
+
         });
 
         return view;
